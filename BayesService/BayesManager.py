d3b489108806128a78a2710753f3a6be9b792921
Fevzihan Yusuf Bozkurt <105756054+efexunn@users.noreply.github.com>
2023-12-20 15:25:34 +0300

    graph added.

--- BayesService/BayesManager.py
+++ BayesService/BayesManager.py
@@ -1,7 +1,9 @@
 import math
+import random
+import matplotlib.pyplot as plt
+import numpy as np
 class BayesManager:
     bayesDict = {}
-
     def __init__(self, cultureArtFile, healthFile, politicsFile, sportsFile, wordList, wordDict):
         self.cultureArtFile = cultureArtFile
         self.healthFile = healthFile
@@ -58,10 +60,10 @@ class BayesManager:
         politicsScore += (self.politicsFileLength / self.sumOfLengths)
         sportsScore += (self.sportsFileLength / self.sumOfLengths)
 
-        cultureScore = math.log(cultureScore, 10)
-        healthScore = math.log(healthScore, 10)
-        politicsScore = math.log(politicsScore, 10)
-        sportsScore = math.log(sportsScore, 10)
+        cultureScore = math.log(cultureScore)
+        healthScore = math.log(healthScore)
+        politicsScore = math.log(politicsScore)
+        sportsScore = math.log(sportsScore)
 
         maxScore = max(cultureScore, healthScore, politicsScore, sportsScore)
 
@@ -75,10 +77,48 @@ class BayesManager:
             return "Sports"
 
 
+    def CreateBarChartByBayes(self,bayesDict):
+        # Veri yapısı
+        data = {}
+        for i in range(10):
+            rand = random.randint(1,4200)
+            data[self.wordList[rand]] = {
+                'culture' : bayesDict[self.wordList[rand]]['culture'],
+                'health': bayesDict[self.wordList[rand]]['health'],
+                'politics': bayesDict[self.wordList[rand]]['politics'],
+                'sports': bayesDict[self.wordList[rand]]['sports']
+            }
+        # Kategoriler
+        categories = list(data.keys())
+
+        # Her bir kelimenin kategorideki değerleri
+        culture_values = [data[word]['culture'] for word in categories]
+        health_values = [data[word]['health'] for word in categories]
+        politics_values = [data[word]['politics'] for word in categories]
+        sports_values = [data[word]['sports'] for word in categories]
+
+        bar_width = 0.1
+        index = np.arange(len(categories))
+
+        plt.bar(index, culture_values, bar_width, label='culture')
+        plt.bar(index + bar_width, health_values, bar_width, label='health')
+        plt.bar(index + 2 * bar_width, politics_values, bar_width, label='politics')
+        plt.bar(index + 3 * bar_width, sports_values, bar_width, label='sports')
+
+        plt.xlabel('Words')
+        plt.ylabel('Count')
+        plt.title('Category Counts by Word')
+
+        plt.xticks(index + 1.5 * bar_width, categories)  # Kategorilerin ortasına yerleştir
+        # plt.figure(figsize=(8, 4))
+        plt.legend()
+        plt.show()
+
     def FindCategoryOfText(self, testList):
         judgementList = []
         for i in range(len(testList)):
             judgementList.append(f"Metin {i+1} Türü : {self.CalculateJudgementScores(testList[i])}")
 
+        self.CreateBarChartByBayes(self.ListAllTextsBayesianModel())
         return judgementList
 
